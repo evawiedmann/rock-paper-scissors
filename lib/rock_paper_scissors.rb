5bbfcf299321f89e168ea3ab3bb1923fc289d9a8
Student <example@example.com>
2019-12-11 11:47:19 -0800

    not working

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -6,14 +6,34 @@ class Game
   }
 
   def initialize(input)
-    @input = input
-    @random = rand(3)
+    @input = '1'
+    @input2 = '2'
+    @input3 = '3'
   end
 
   def throw(input)
     # @random = rand(1..3).to_s
-    # if @@weapons.fetch(@input) == "1"
-      return true
-
+    @random1 = '1'
+    @random2 = '2'
+    @random3 = '3'
+    if (@@weapons.fetch(@input) == 'rock') & (@@weapons.fetch(@random2) =='scissor')
+      puts "rock wins"
+      return 'rock wins'
+    elsif (@@weapons.fetch(@input) == 'rock') & (@@weapons.fetch(@random3) =='paper')
+      puts "paper wins"
+      return  "paper wins"
+    elsif (@@weapons.fetch(@input) == 'rock') & (@@weapons.fetch(@random1) =='rock')
+      puts "tie"
+      return  "tie"
+    elsif (@@weapons.fetch(@input2) == 'scissors') & (@@weapons.fetch(@random1) =='rock')
+      puts "rock wins"
+      return  "rock wins"
+    elsif (@@weapons.fetch(@input2) == 'scissors') & (@@weapons.fetch(@random3) =='paper')
+      puts "scissors wins"
+      return  "scissors"
+    elsif (@@weapons.fetch(@input2) == 'scissors') & (@@weapons.fetch(@random2) =='scissors')
+      puts "tie"
+      return  "tie"
+    end
   end
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -3,8 +3,28 @@ require('rspec')
 
 describe('Game#throw') do
 
-    it("returns true if rock is the object and scissors is the argument") do
-      game = Game.new('rock')
-      expect(game.throw('scissors')).to(eq(true))
+  it("returns true if rock is the object and scissors is the argument") do
+    game = Game.new('rock')
+    expect(game.throw('scissors')).to(eq("rock wins"))
+  end
+  it('paper beats rock') do
+    game = Game.new('rock')
+    expect(game.throw('paper')).to(eq("paper wins"))
+  end
+  it('rock ties rock') do
+    game = Game.new('rock')
+    expect(game.throw('rock')).to(eq("tie"))
+  end
+  it('rock beats scissors') do
+    game = Game.new('scissors')
+    expect(game.throw('rock')).to(eq("rock wins"))
+  end
+  it('scissors beats paper') do
+    game = Game.new('scissors')
+    expect(game.throw('paper')).to(eq("scissors wins"))
+  end
+  it('scissors ties scissors') do
+    game = Game.new('scissors')
+    expect(game.throw('scissors')).to(eq("tie"))
   end
 end
